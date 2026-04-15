fee7324a04e7ffcbac06195409bb5f1d0f95af4b
Nagamani53 <23je0949@iitism.ac.in>
2026-04-15 19:31:34 +0530

    updated code
    Refactor lottery process simulation to use a struct for process management and improve readability. Added lottery draw function for selecting a winning process based on ticket distribution.

--- G1_Project2_2/nagamani/lottery.c
+++ G1_Project2_2/nagamani/lottery.c
@@ -2,46 +2,88 @@
 #include <stdlib.h>
 #include <time.h>
 
+struct Process {
+    int pid;
+    int bt;       
+    int tickets;  
+    int wt;       
+    int tat;      
+    int remaining; 
+    int done;     
+};
+
+// Lottery draw: returns index of winning process
+int lotteryDraw(struct Process p[], int n, int totalTickets) {
+    int winning = (rand() % totalTickets) + 1;
+    int cumulative = 0;
+    for (int i = 0; i < n; i++) {
+        if (!p[i].done) {
+            cumulative += p[i].tickets;
+            if (winning <= cumulative)
+                return i;
+        }
+    }
+    return -1;
+}
+
 int main() {
-    int n, i, total_tickets = 0, winner;
-    int bt[10], tickets[10], wt[10], tat[10];
+    int n;
+    srand(time(0));
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    for(i = 0; i < n; i++) {
-        printf("Enter burst time for process %d: ", i+1);
-        scanf("%d", &bt[i]);
+    struct Process p[n];
 
-        printf("Enter tickets for process %d: ", i+1);
-        scanf("%d", &tickets[i]);
-
-        total_tickets += tickets[i];
+    for (int i = 0; i < n; i++) {
+        p[i].pid = i + 1;
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &p[i].bt);
+        printf("Enter tickets for process %d: ", i + 1);
+        scanf("%d", &p[i].tickets);
+        p[i].remaining = p[i].bt;
+        p[i].wt  = 0;
+        p[i].tat = 0;
+        p[i].done = 0;
     }
 
-    srand(time(0));
-    winner = rand() % total_tickets;
+    int currentTime = 0;
+    int completed = 0;
 
-    int sum = 0;
-    int selected = -1;
+    // Calculate total tickets (only undone processes)
+    while (completed < n) {
+        int totalTickets = 0;
+        for (int i = 0; i < n; i++)
+            if (!p[i].done)
+                totalTickets += p[i].tickets;
 
-    for(i = 0; i < n; i++) {
-        sum += tickets[i];
-        if(winner < sum) {
-            selected = i;
-            break;
-        }
+        int idx = lotteryDraw(p, n, totalTickets);
+        if (idx == -1) break;
+
+        printf("\nSelected Process: P%d\n", p[idx].pid);
+
+        // Run selected process to completion (non-preemptive)
+        p[idx].wt  = currentTime;
+        currentTime += p[idx].bt;
+        p[idx].tat = currentTime;
+        p[idx].done = 1;
+        completed++;
     }
 
-    printf("\nSelected Process: P%d\n", selected + 1);
-
-    wt[selected] = 0;
-    tat[selected] = bt[selected];
-
+    // Display Results
     printf("\nProcess\tBT\tWT\tTAT\n");
-    for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\n", i+1, bt[i], wt[i], tat[i]);
+    float avgWT = 0, avgTAT = 0;
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\n", p[i].pid, p[i].bt, p[i].wt, p[i].tat);
+        avgWT  += p[i].wt;
+        avgTAT += p[i].tat;
     }
+
+    printf("\nAverage Waiting Time    : %.2f\n", avgWT / n);
+    printf("Average Turnaround Time : %.2f\n", avgTAT / n);
 
     return 0;
 }
+   
+
+   
